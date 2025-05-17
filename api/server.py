41716585408cf64306ada28ded4ebf812fb3b78d
Conor McFeely <cwmcfeely@gmail.com>
2025-05-17 11:25:52 +0100

    Adding the Execution report for trades completed, Adding the functionality to cancel my strategy orders. Improve the logging to be by strategy

--- api/server.py
+++ api/server.py
@@ -8,5 +8,10 @@ logger.propagate = False
 app = create_app()
 
 if __name__ == "__main__":
-    setup_logging(level=logging.DEBUG)  # Initialize once
+    setup_logging(
+        level=logging.INFO,
+        app_log_file="logs/app.debug.log",
+        fix_server_log_file="logs/fix_server.log",
+        strategies=["my_strategy", "passive_liquidity_provider", "market_maker", "momentum"]
+    )
     app.run(host="127.0.0.1", port=5000, debug=False)
